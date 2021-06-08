9bb80f925c9fe87a94a7140d75bb57af7af62d78
Josip Djolonga <josipd@google.com>
2021-06-08 11:06:26 -0700

    Internal change
    PiperOrigin-RevId: 378197195

--- robustness_metrics/__init__.py
+++ robustness_metrics/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A library for evaluating the robustness of machine learning models."""
+"""A library for evaluating the robustness of machine learning models.  """
 
 from robustness_metrics import common
 from robustness_metrics import datasets
